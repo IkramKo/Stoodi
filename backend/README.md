648e636db09214ade989db74f352b203c5370e98
Johnatan Gao <dev@johnatangao.com>
2023-02-04 17:05:40 -0500

    LOL

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,6 @@
 # Backend
+
+
+1. pip install -r requirements.txt
+2. python -m nltk.downloader punkt
+3. cd backend/ && uvicorn main:app --reload
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
