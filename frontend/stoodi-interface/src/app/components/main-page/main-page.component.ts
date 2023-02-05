9d0141f77c26de0fefdb08082aabc31355076c23
Johnatan Gao <dev@johnatangao.com>
2023-02-05 09:48:20 -0500

    nice

--- frontend/stoodi-interface/src/app/components/main-page/main-page.component.ts
+++ frontend/stoodi-interface/src/app/components/main-page/main-page.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+declare let particlesJS: any;
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   title = 'stoodi-interface';
   transcript = '';
 
+  ngOnInit(): void {
+    this.displayParticles();
+  }
+
+  private displayParticles() : void {    
+    particlesJS.load('particles-js', '../assets/json/stars-particles.json', () => { console.log('Star particles loaded.') });
+  }
   updateContent(content: string) {
     this.transcript = content;
   }
